19fed55644ba166ab668b61534399f0541a778c2
Arnav-Bista <arnav.bista12@gmail.com>
2025-03-23 16:46:15 +0000

    WIP Card

--- lib/features/occupancy/presentation/screens/occupancy_screen.dart
+++ lib/features/occupancy/presentation/screens/occupancy_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:occupancy_frontend/constants.dart';
 import 'package:occupancy_frontend/features/occupancy/presentation/widgets/current_time.dart';
 import 'package:occupancy_frontend/features/occupancy/presentation/widgets/occupancy_card.dart';
+import 'package:occupancy_frontend/features/occupancy/presentation/widgets/occupancy_card_wip.dart';
 
 class OccupancyScreen extends StatelessWidget {
   const OccupancyScreen({super.key});
@@ -28,15 +29,21 @@ class OccupancyScreen extends StatelessWidget {
                   displayName: "The Gym",
                   dataName: "gym",
                 ),
-                OccupancyCard(
+                OccupancyCardWIP(
                   width: constraints.maxWidth * 0.7,
                   progressHeight: constraints.maxHeight * 0.3,
                   cardColor: const Color(ConstantColors.burgundy),
-                  progressColor: const Color(0XFFFFD6BA),
-                  emptyColor: Colors.grey[500]!,
-                  displayName: "Main Library",
-                  dataName: "main_library",
-                ),
+                  displayName: "The Labs",
+                )
+                // OccupancyCard(
+                //   width: constraints.maxWidth * 0.7,
+                //   progressHeight: constraints.maxHeight * 0.3,
+                //   cardColor: const Color(ConstantColors.burgundy),
+                //   progressColor: const Color(0XFFFFD6BA),
+                //   emptyColor: Colors.grey[500]!,
+                //   displayName: "Main Library",
+                //   dataName: "main_library",
+                // ),
               ],
             ),
           );
